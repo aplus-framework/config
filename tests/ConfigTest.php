49e922600ac5ba6c92ae4ef87576a3c3af66bcfc
Natan Felles <natanfelles@gmail.com>
2021-05-30 08:54:34 -0300

    Import Config from MVC Library

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -7,16 +7,55 @@ class ConfigTest extends TestCase
 {
 	protected Config $config;
 
-	public function setup() : void
+	protected function setUp() : void
 	{
-		$this->config = new Config();
+		$this->config = new Config(__DIR__ . '/configs');
 	}
 
-	public function testConfig()
+	public function testLoadException()
 	{
-		$this->assertEquals(
-			'Framework\Config\Config::test',
-			$this->config->test()
-		);
+		$this->expectException(\LogicException::class);
+		$this->expectExceptionMessage('Config file not found: bazz');
+		$this->config->load('bazz');
+	}
+
+	public function testSetDirException()
+	{
+		$this->expectException(\LogicException::class);
+		$this->expectExceptionMessage('Config directory not found: ' . __DIR__ . '/unknown');
+		(new Config(__DIR__ . '/unknown'));
+	}
+
+	public function testGetAll()
+	{
+		$this->assertEquals([], $this->config->getAll());
+		$this->config->load('bar');
+		$this->assertEquals([
+			'bar' => [
+				'default' => [
+				],
+			],
+		], $this->config->getAll());
+		$this->config->load('foo');
+		$this->assertEquals([
+			'bar' => [
+				'default' => [
+				],
+			],
+			'foo' => [
+				'default' => [
+				],
+			],
+		], $this->config->getAll());
+	}
+
+	public function testAdd()
+	{
+		$this->config->add('foo', ['baz']);
+		$this->assertEquals(['baz'], $this->config->get('foo'));
+		$this->config->set('foo', ['bar']);
+		$this->assertEquals(['bar'], $this->config->get('foo'));
+		$this->config->add('foo', ['baz', 'hi']);
+		$this->assertEquals(['baz', 'hi'], $this->config->get('foo'));
 	}
 }
