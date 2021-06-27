5bff89d73b0b6b80fd8bb6754a58230a98e4513d
Natan Felles <natanfelles@gmail.com>
2021-06-27 13:09:48 -0300

    Coding Standard: Add header comments

--- src/Parsers/Parser.php
+++ src/Parsers/Parser.php
@@ -1,4 +1,13 @@
-<?php namespace Framework\Config\Parsers;
+<?php
+/*
+ * This file is part of The Framework Config Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\Config\Parsers;
 
 use LogicException;
 
