d1c62733038afeffd86e42fcc6f78963973010f0
Natan Felles <natanfelles@gmail.com>
2021-08-17 09:28:42 -0300

    Add JSON Parser

---
+++ tests/Parsers/JsonParserTest.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of Aplus Framework Config Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Config\Parsers;
+
+use Framework\Config\Parsers\JsonParser;
+
+final class JsonParserTest extends ParserTestCase
+{
+    protected mixed $config = __DIR__ . '/../configs/config.json';
+    protected string $parserClass = JsonParser::class;
+}
